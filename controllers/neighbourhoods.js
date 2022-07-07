06318e8b8cb39d46493a911158fbfd4e08bc0031
caitbrock <cait2brock@gmail.com>
2022-07-07 12:17:45 -0400

    Heroku Setup complete and functional

--- controllers/neighbourhoods.js
+++ controllers/neighbourhoods.js
@@ -1,12 +1,11 @@
-const Neighbourhood = require("../models/neighbourhood.js");
 // const developmentData = require("../public/assets/ACTIVE - North Toronto - Neighbourhood 173.json")
 // JSON.stringify(developmentData);
 const Development = require("../models/development");
 
 async function index(req, res) {
   const developmentData = await Development.find();
-
   res.render("neighbourhoods", { developmentData });
+  console.log("this is my data", developmentData);
 }
 
 module.exports = {
