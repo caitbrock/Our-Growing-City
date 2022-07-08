d36352107dab2c646fc561f33ff2054b21ab7d73
caitbrock <cait2brock@gmail.com>
2022-07-08 13:30:15 -0400

    User information running

--- models/user.js
+++ models/user.js
@@ -5,7 +5,16 @@ var userSchema = new Schema({
   name: String,
   email: String,
   googleId: String,
-  admin: Boolean,
+  avatar: {
+    type: String,
+    default: "",
+    required: false,
+  },
+  admin: {
+    type: Boolean,
+    required : true,
+    default: false
+},
 });
 
 module.exports = mongoose.model("user", userSchema);
